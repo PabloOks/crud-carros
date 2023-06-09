83d842a876bf69a398010bb952cfcd05d72948b2
= <=>
2023-06-09 12:20:24 -0400

    update(request): Change authorized() to true update(route): Change restore route

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('cars', CarController::class);
-Route::get('cars/{car}?deleted=true', [CarController::class, 'show'])->withTrashed();
+Route::get('cars/{car}/deleted', [CarController::class, 'show'])->withTrashed();
 Route::post('cars/{car}/restore', [CarController::class, 'restore'])->withTrashed();
